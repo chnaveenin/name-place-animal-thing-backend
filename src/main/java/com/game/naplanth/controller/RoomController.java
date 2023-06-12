19f89f51ec780d347006a55b91d30a2617a114fa
chnaveenin <chnaveen.in@gmail.com>
2023-06-12 15:17:54 +0530

    initialised backend application

--- src/main/java/com/game/naplanth/controller/RoomController.java
+++ src/main/java/com/game/naplanth/controller/RoomController.java
@@ -3,26 +3,40 @@ package com.game.naplanth.controller;
 import com.game.naplanth.RoomRepo;
 import com.game.naplanth.model.Room;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="*")
 public class RoomController {
 
     @Autowired
     RoomRepo repo;
 
-    @GetMapping(value="/")
+    @RequestMapping(value="/")
     public String test() {
         return "Working";
     }
 
     @GetMapping(value="/rooms")
     public List<Room> getAllRooms() {
+        System.out.println("in rooms");
+        List<Room> rs = repo.findAll();
+        System.out.println(rs);
         return repo.findAll();
     }
 
+    @GetMapping(value="/insert")
+    public void insert() {
+        Room temp = new Room();
+        temp.setRoomid("hello");
+        String[] st = {"gani", "naveen"};
+        temp.setParticipants(st);
+        repo.insert(temp);
+    }
 }
